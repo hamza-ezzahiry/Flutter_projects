edda397a7d2a10a53a03c364ccb6a3b7a7edfd34
hamza-ezzahiry <hamza.ezzahiry@gmail.com>
2020-09-12 22:40:44 +0100

    add package of audio

--- xylophone-flutter/lib/main.dart
+++ xylophone-flutter/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  static AudioCache player = new AudioCache();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
