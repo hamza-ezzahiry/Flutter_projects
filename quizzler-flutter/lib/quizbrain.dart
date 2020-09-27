0302b7d2ad95430df7f36e7a37c0fb5699f0e548
hamza-ezzahiry <hamza.ezzahiry@gmail.com>
2020-09-27 14:41:02 +0100

    make a class of question's list

---
+++ quizzler-flutter/lib/quizbrain.dart
@@ -0,0 +1,16 @@
+import 'questions.dart';
+
+class QuizBrain {
+  List<Question> listOfQuestions = [
+    Question(
+        q: 'Only character or integer can be used in switch statement',
+        a: false),
+    Question(
+        q: '#define is known as preprocessor compiler directive.', a: true),
+    Question(
+        q: 'Algorithm is the graphical representation of logic.', a: false),
+    Question(
+        q: 'sizeof( ) is a function that returns the size of a variable.',
+        a: false),
+  ];
+}
