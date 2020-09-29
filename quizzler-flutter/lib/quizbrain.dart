9c6b5d63c3674ccc36c8ebe41747197a224ba814
hamza-ezzahiry <hamza.ezzahiry@gmail.com>
2020-09-29 20:48:18 +0100

    make the list private

--- quizzler-flutter/lib/quizbrain.dart
+++ quizzler-flutter/lib/quizbrain.dart
@@ -1,7 +1,7 @@
 import 'questions.dart';
 
 class QuizBrain {
-  List<Question> listOfQuestions = [
+  List<Question> _listOfQuestions = [
     Question(
         q: 'Only character or integer can be used in switch statement',
         a: false),
@@ -13,4 +13,11 @@ class QuizBrain {
         q: 'sizeof( ) is a function that returns the size of a variable.',
         a: false),
   ];
+  String getQuestion(int nb_question) {
+    return (_listOfQuestions[nb_question].questiontext);
+  }
+
+  bool getAnswer(int nb_question) {
+    return (_listOfQuestions[nb_question].answer);
+  }
 }
